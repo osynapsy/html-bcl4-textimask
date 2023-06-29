7e31ca778ab7389a2e22a17f202bfb46ad2bb627
Pietro Celeste <pietro.celeste@gmail.com>
2023-06-29 11:58:54 +0200

    First commit

---
+++ src/TextIMaskBox.php
@@ -0,0 +1,58 @@
+<?php
+namespace Osynapsy\Bcl4\TextIMaskBox;
+
+use Osynpasy\Bcl4\TextBox;
+
+/**
+ * Description of TextIMaskBox
+ *
+ * @author peter
+ */
+class TextIMaskBox extends TextBox
+{
+    const IMASK_INTEGER = 10;
+    const IMASK_NUMBER = 11;
+    const IMASK_CURRENCY = 12;
+    const IMASK_DATE = 20;
+
+    protected $imask = [
+        self::IMASK_INTEGER => [
+            'id' => 'Number',
+            'class' => 'text-right'
+        ],
+        self::IMASK_NUMBER  => [
+            'id' => 'Number',
+            'class' => 'text-right'
+        ],
+        self::IMASK_CURRENCY => [
+            'id' => 'Currency',
+            'class' => 'text-right'
+        ],
+        self::IMASK_DATE => [
+            'id' => 'Date',
+            'class' => 'text-center'
+        ]
+    ];
+
+    public function __construct($name, $class = '')
+    {
+        parent::__construct($name, $class);
+        $this->requireJs('lib/imask-6.0.5/imask.js');
+        $this->requireJs('bcl4/textimask/script.js');
+    }
+
+    public function setIMask($maskId)
+    {
+        $this->validateMaskId($maskId);
+        $mask = $this->imask[$maskId];
+        $this->addClass(trim('input-mask '.$mask['class']));
+        $this->attribute('data-imask', $mask['id']);
+    }
+
+    protected function validateMaskId($maskId)
+    {
+        if (!array_key_exists($maskId, $this->imask)) {
+            throw new \Exception("TextBox {$this->id} : iMask format {$maskId} not regnized");
+        }
+    }
+}
